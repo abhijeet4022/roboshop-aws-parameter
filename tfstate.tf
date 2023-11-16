38f31999463e4c5a7e9bd7241defae447cfa04e6
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-16 11:42:35 +0530

    Terraform: Roboshop-App Terraform

--- tfstate.tf
+++ tfstate.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "roboshop-statefile"
-    key    = "roboshop-app/dev/terraform.tfstate"
+    key    = "aws-parameter/terraform.tfstate"
     region = "us-east-1"
   }
 }
